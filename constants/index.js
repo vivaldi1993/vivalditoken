f2f3a5d1232254a628c8179cb6446dc4363ef44f
vivaldi1993 <140600744+vivaldi1993@users.noreply.github.com>
2023-07-31 13:08:53 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xfa082be52cb73bbaec67017db77950ed8eb97d40";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
